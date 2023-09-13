79188d9a44fec5f9722ac05b785572f7e3801743
akmalahmed125 <akmalahmed125@gmail.com>
2023-09-13 20:23:23 +0300

    push changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,25 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:marquee/marquee.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (Platform.isAndroid) {
-    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
-  }
-
-  runApp(MaterialApp(
-      home: new MyApp()
-  ));
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => new _MyAppState();
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
-  final GlobalKey webViewKey = GlobalKey();
-
-  InAppWebViewController? webViewController;
-  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
-      crossPlatform: InAppWebViewOptions(
-        useShouldOverrideUrlLoading: true,
-        mediaPlaybackRequiresUserGesture: false,
-      ),
-      android: AndroidInAppWebViewOptions(
-        useHybridComposition: true,
-      ),
-      ios: IOSInAppWebViewOptions(
-        allowsInlineMediaPlayback: true,
-      ));
-
-  late PullToRefreshController pullToRefreshController;
-  String url = "";
-  double progress = 0;
-  final urlController = TextEditingController();
-
   @override
   void initState() {
     super.initState();
-
-    pullToRefreshController = PullToRefreshController(
-      options: PullToRefreshOptions(
-        color: Colors.blue,
-      ),
-      onRefresh: () async {
-        if (Platform.isAndroid) {
-          webViewController?.reload();
-        } else if (Platform.isIOS) {
-          webViewController?.loadUrl(
-              urlRequest: URLRequest(url: await webViewController?.getUrl()));
-        }
-      },
-    );
   }
 
   @override
@@ -69,124 +29,160 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    return bottom_bar();
+  }
+
+  // ignore: non_constant_identifier_names
+  Scaffold bottom_bar() {
     return Scaffold(
-          appBar: AppBar(title: Text("")),
-          body: SafeArea(
-              child: Column(children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search)
-                  ),
-                  controller: urlController,
-                  keyboardType: TextInputType.url,
-                  onSubmitted: (value) {
-                    var url = Uri.parse(value);
-                    if (url.scheme.isEmpty) {
-                      url = Uri.parse("https://www.google.com/search?q=");
-                    }
-                    webViewController?.loadUrl(
-                        urlRequest: URLRequest(url: url));
-                  },
-                ),
-                Expanded(
-                  child: Stack(
-                    children: [
-                      InAppWebView(
-                        key: webViewKey,
-                        initialUrlRequest:
-                        URLRequest(url: Uri.parse("http://www.darulehsan.pk/")),
-                        initialOptions: options,
-                        pullToRefreshController: pullToRefreshController,
-                        onWebViewCreated: (controller) {
-                          webViewController = controller;
-                        },
-                        onLoadStart: (controller, url) {
-                          setState(() {
-                            this.url = url.toString();
-                            urlController.text = this.url;
-                          });
-                        },
-                        androidOnPermissionRequest: (controller, origin, resources) async {
-                          return PermissionRequestResponse(
-                              resources: resources,
-                              action: PermissionRequestResponseAction.GRANT);
-                        },
-                        shouldOverrideUrlLoading: (controller, navigationAction) async {
-                          var uri = navigationAction.request.url!;
+      body: Column(
+        children: [
+          titleSection,
+          MarqueeWidget,
+          _booksTitle("Maqalat-e-Hikmat")
+        ],
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _pushAddTodoScreen,
+      //   tooltip: 'Increment',
+      //   elevation: 8.0,
+      //   child: const Icon(Icons.add),
+      // ),
+      bottomNavigationBar: BottomAppBar(
+        height: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // ignore: avoid_print
+            Expanded(
+              child: IconButton(
+                icon: const Icon(Icons.book_online_rounded),
+                onPressed: () => print('hello world'),
+              ),
+            ),
+            // ignore: avoid_print
+            Expanded(
+              child: IconButton(
+                icon: const Icon(Icons.settings_applications),
+                onPressed: () => print('hello world'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
 
-                          if (![ "http", "https", "file", "chrome",
-                            "data", "javascript", "about"].contains(uri.scheme)) {
-                            if (await canLaunchUrl(uri)) {
-                              // Launch the App
-                              await launchUrl(
-                                uri,
-                              );
-                              // and cancel the request
-                              return NavigationActionPolicy.CANCEL;
-                            }
-                          }
+  void _pushAddTodoScreen() {
+    // ignore: avoid_print
+    print("hello floating");
+  }
 
-                          return NavigationActionPolicy.ALLOW;
-                        },
-                        onLoadStop: (controller, url) async {
-                          pullToRefreshController.endRefreshing();
-                          setState(() {
-                            this.url = url.toString();
-                            urlController.text = this.url;
-                          });
-                        },
-                        onLoadError: (controller, url, code, message) {
-                          pullToRefreshController.endRefreshing();
-                        },
-                        onProgressChanged: (controller, progress) {
-                          if (progress == 100) {
-                            pullToRefreshController.endRefreshing();
-                          }
-                          setState(() {
-                            this.progress = progress / 100;
-                            urlController.text = url;
-                          });
-                        },
-                        onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                          setState(() {
-                            this.url = url.toString();
-                            urlController.text = this.url;
-                          });
-                        },
-                        onConsoleMessage: (controller, consoleMessage) {
-                          print(consoleMessage);
-                        },
-                      ),
-                      progress < 1.0
-                          ? LinearProgressIndicator(value: progress)
-                          : Container(),
-                    ],
-                  ),
-                ),
-                ButtonBar(
-                  alignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ElevatedButton(
-                      child: Icon(Icons.arrow_back),
-                      onPressed: () {
-                        webViewController?.goBack();
-                      },
+  Widget titleSection = Container(
+    margin: const EdgeInsets.only(top: 0),
+    color: const Color.fromRGBO(248, 147, 0, 1),
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        /*1*/
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // ignore: sized_box_for_whitespace
+            Container(
+              width: 100,
+              height: 40,
+              margin: const EdgeInsets.only(top: 30),
+              child: Image.asset('images/Yaqayyum.png'),
+            )
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // ignore: sized_box_for_whitespace
+            Container(
+                width: 160,
+                height: 40,
+                margin: const EdgeInsets.only(top: 20),
+                child: Image.asset('images/bismillah.png')),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            // ignore: sized_box_for_whitespace
+            Container(
+                width: 100,
+                height: 40,
+                margin: const EdgeInsets.only(top: 30),
+                child: Image.asset('images//Yahayyo.png')),
+          ],
+        )
+      ],
+    ),
+  );
+  // ignore: non_constant_identifier_names, avoid_unnecessary_containers
+  Widget MarqueeWidget = Container(
+    color: const Color.fromRGBO(236, 196, 166, 100),
+    padding: const EdgeInsets.only(top: 10, bottom: 20),
+    child: Row(
+      children: [
+        Expanded(
+          child: SizedBox(
+            height: 40,
+            child: Marquee(
+              text:
+                  'الهم صلي علي سيدنا محمد النبي الامي و علي اله و اصحابه و عترته بعدد كل معلوم لك و استغفرالله الذي لا اله الا هو الحي القيوم و اتوب اليه يا حي يا قيوم',
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.green),
+              scrollAxis: Axis.horizontal,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              textDirection: TextDirection.rtl,
+              blankSpace: 400.0,
+              velocity: 50.0,
+              pauseAfterRound: const Duration(seconds: 1),
+              startPadding: 20.0,
+              accelerationDuration: const Duration(seconds: 1),
+              accelerationCurve: Curves.linear,
+              decelerationDuration: const Duration(milliseconds: 500),
+              decelerationCurve: Curves.easeOut,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+  // ignore: non_constant_identifier_names
+  _booksTitle(title) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      //padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.only(top: 20, left: 1, right: 10),
+      child: Text(
+                title,
+                textAlign: TextAlign.left,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  //decoration:TextDecoration.underline,
+                  height: 2,
+                  //leadingDistribution: TextLeadingDistribution.even,
+                  fontFamily: "Agane55",
+                  color: Colors.black,
+                  shadows: [
+                    Shadow(
+                      color: Colors.amber,
                     ),
-                    ElevatedButton(
-                      child: Icon(Icons.arrow_forward),
-                      onPressed: () {
-                        webViewController?.goForward();
-                      },
-                    ),
-                    ElevatedButton(
-                      child: Icon(Icons.refresh),
-                      onPressed: () {
-                        webViewController?.reload();
-                      },
-                    ),
+                    Shadow(
+                      color: Colors.greenAccent,
+                    )
                   ],
                 ),
-              ]))
+              ),
     );
   }
 }

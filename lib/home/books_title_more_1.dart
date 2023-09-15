7cefff0edf27d215475461e4546191396e6570af
akmalahmed125 <akmalahmed125@gmail.com>
2023-09-16 02:08:38 +0300

    home page ready

--- lib/home/books_title_more_1.dart
+++ lib/home/books_title_more_1.dart
@@ -7,54 +7,51 @@ const suburl = "assets/img";
 // ignore: unused_element
 
 makalatBooksList(bookName, totalChapters) {
-  return Container(
-      margin: const EdgeInsets.symmetric(vertical: 20),
-      height: 200,
-      child: Directionality(
+  return SliverToBoxAdapter(
+    child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 20),
+        height: 180,
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: ListView.builder(
+              // This next line does the trick.
+              scrollDirection: Axis.horizontal,
+              itemCount: totalChapters,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return const SizedBox();
+                }
+                if (index >= 1 && index <= 9) {
+                  return Container(
+                    margin: const EdgeInsets.all(5),
+                    width: 150,
+                    child: Card(
+                      elevation: 5,
+                      margin: const EdgeInsets.all(2),
+                      child: Image.network(
+                        // ignore: prefer_interpolation_to_compose_strings
+                        '${'$url/$suburl/' + bookName}0$index.jpg',
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container(
+                    margin: const EdgeInsets.all(5),
+                    width: 150,
+                    child: Card(
+                      elevation: 5,
+                      margin: const EdgeInsets.all(2),
+                      child: Image.network(
+                        // ignore: prefer_interpolation_to_compose_strings
+                        '${'$url/$suburl/' + bookName}$index.jpg',
+                      ),
+                    ),
+                  );
+                }
+              }
+              // ignore: avoid_unnecessary_containers
 
-        textDirection: TextDirection.rtl,
-        child: ListView.builder(
-            // This next line does the trick.
-            scrollDirection: Axis.horizontal,
-            itemCount: totalChapters,
-            itemBuilder: (BuildContext context, int index) {
-              if(index == 0){
-                return const SizedBox(); 
-              }
-              if (index >= 1 && index <= 9) {
-                return Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 150,
-                  child: Row(
-                    children: <Widget>[
-                      Image.network(
-                        // ignore: prefer_interpolation_to_compose_strings
-                        '${'$url/$suburl/'+bookName}0$index.jpg',
-                      )
-                      
-                    ],
-                  ),
-                  
-                );
-              }else{
-                return Container(
-                  margin: const EdgeInsets.all(10),
-                  width: 150,
-                  child: Row(
-                    children: <Widget>[
-                      Image.network(
-                        // ignore: prefer_interpolation_to_compose_strings
-                        '${'$url/$suburl/'+bookName}$index.jpg',
-                      )
-                      
-                    ],
-                  ),
-                  
-                );
-              }
-            }
-            // ignore: avoid_unnecessary_containers
-      
-            ),
-      ));
+              ),
+        )),
+  );
 }
